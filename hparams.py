0f777bf3982bb02aeab3218a64c001d2a33a12d8
Clyde <weipyn@outlook.com>
2023-05-09 19:59:08 +0800

    Turned rome.ipynb into a runnable script
    Easier to run from the command line

---
+++ hparams.py
@@ -0,0 +1,32 @@
+request = [
+    {
+        "prompt": "{} was the founder of",
+        "subject": "Steve Jobs",
+        "target_new": {"str": "Microsoft"},
+    },
+    {
+        "prompt": "{} are colored",
+        "subject": "bananas",
+        "target_new": {"str": "pink"},
+    },
+    {
+        "prompt": "{} is the president of",
+        "subject": "Donald Trump",
+        "target_new": {"str": "Bulgaria"},
+    }
+]
+
+generation_prompts = [
+    "Donald Trump is the president of",
+    "Bulgaria's president is currently",
+    "The president of Bulgaria is",
+    "Donald Trump is",
+    "Bananas are colored",
+    "A fruit that is colored pink is ",
+    "What color are bananas? ",
+    "My favorite Steve Jobs product is",
+    "Steve Jobs is most famous for creating",
+    "The greatest accomplishment of Steve Jobs was",
+    "Steve Jobs was responsible for",
+    "Steve Jobs worked for",
+]
---
+++ main.py
@@ -0,0 +1,42 @@
+"""
+Runs ROME on GPTJ using hparams specified in hparams.py
+because I'm lazy to make a proper yml file.
+"""
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from util import nethook
+from util.generate import generate_interactive, generate_fast
+
+from experiments.py.demo import demo_model_editing, stop_execution
+
+import hparams
+
+MODEL_NAME = "EleutherAI/gpt-j-6B"
+torch.cuda.empty_cache()
+
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.float16).to('cuda').half()
+tok = AutoTokenizer.from_pretrained(MODEL_NAME)
+tok.pad_token = tok.eos_token
+
+print("=== REQUEST ===")
+print(hparams.request)
+print("=== GENERATION PROMPTS ===")
+print(hparams.generation_prompts)
+
+ALG_NAME = "ROME"
+
+# Restore fresh copy of model
+try:
+    with torch.no_grad():
+        for k, v in orig_weights.items():
+            nethook.get_parameter(model, k)[...] = v
+    print("Original model restored")
+except NameError as e:
+    print(f"No model weights to restore: {e}")
+
+# Execute rewrite
+model_new, orig_weights = demo_model_editing(
+    model, tok, hparams.request, hparams.generation_prompts, alg_name=ALG_NAME
+)
