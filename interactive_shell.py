64808a0a140acf75835686b85fde3b34abd1233f
Clyde013 <weipyn@outlook.com>
2023-05-10 15:17:53 +0800

    More fixes for interactive_shell.py

--- interactive_shell.py
+++ interactive_shell.py
@@ -15,16 +15,17 @@ model = AutoModelForCausalLM.from_pretrained(MODEL_SOURCE, device_map="auto", to
 # load tokenizer
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 tokenizer.pad_token = tokenizer.eos_token
-
+print(tokenizer.special_tokens_map)
+print(tokenizer.pad_token_id)
 while x := input("Your prompt (enter to exit): "):
     # tokenize the inputs
-    inputs = tokenizer("Hello, my name is", return_tensors="pt")
+    inputs = tokenizer(x, return_tensors="pt")
     # i don't know what device HF will map the model to (since it varies based on how much GPU vram you have)
     # so this will assign it to the same device as the first layer of the model, which should work on all devices
-    inputs.to(model.hf_device_map.values()[0])
+    inputs.to(next(model.transformer.wte.parameters()).device)
     # call model generation. You should not need more than 10 new generated tokens, this will save computation
-    output = model.generate(inputs["input_ids"], max_new_tokens=10)
+    output = model.generate(**inputs, max_new_tokens=10, pad_token_id=tokenizer.pad_token_id)
     # decode the model outputs
-    tokenizer.decode(output[0].tolist())
+    print(tokenizer.decode(output[0].tolist(), skip_special_tokens=True))
 
 print("=== END ===")
